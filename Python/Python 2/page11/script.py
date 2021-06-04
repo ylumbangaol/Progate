355b429f3daf781a2c9842c39b31f5884236d32b
Yosua Lumban Gaol <55227268+ylumbangaol@users.noreply.github.com>
2021-06-05 05:27:45 +0700

    Add files via upload

---
+++ Python/Python 2/page11/script.py
@@ -0,0 +1,9 @@
+# Create a dictionary with keys and values, and assign it to the items variable
+items = {'apple':2, 'banana':4, 'orange':6}
+# Create a for loop that gets the keys of items
+for item_name in items:
+    print(item_name)
+    # Print '--------------------------------------------------'
+    print('--------------------------------------------------')
+    # Print 'Each ___ costs ___ dollars'
+    print('Each ' + item_name + 'costs '+ str(items[item_name]) + ' dollars')
---
+++ Python/Python 2/page8/script.py
@@ -0,0 +1,9 @@
+x = 10
+
+# Create a while loop that repeats while x is greater than 0
+while x > 0:
+    print (x)
+    # Print the x variable 
+    x -= 1
+    # Subtract 1 from the x variable
+    

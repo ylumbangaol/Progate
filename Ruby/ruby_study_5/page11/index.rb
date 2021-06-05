31c4fa2b912c81be62d57f044dfcd0d036fb12c3
Yosua Lumban Gaol <55227268+ylumbangaol@users.noreply.github.com>
2021-06-06 03:12:50 +0700

    Add files via upload

---
+++ Ruby/ruby_study_5/page11/index.rb
@@ -0,0 +1,11 @@
+# Import the Date class using require
+require "date"
+
+# Assign an instance of Date to the birthday variable
+birthday = Date.new(1998, 3, 19)
+
+# Print the birthday variable
+puts birthday
+
+# Print the return value of the sunday? instance method of the birthday variable
+puts birthday.sunday?
---
+++ Ruby/ruby_study_5/page2/food.rb
@@ -0,0 +1,6 @@
+# Import menu.rb using require
+require "./menu"
+
+# Define the Food class that inherits from the Menu class
+class Food < Menu
+end
